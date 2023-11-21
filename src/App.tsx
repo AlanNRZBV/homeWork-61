f756eb034392889509988b1e1dd633dbca3f9bd5
Alan Noruzbaev <alan.noruzbaev@gmail.com>
2023-11-21 19:05:26 +0600

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Container, Row } from 'react-bootstrap';
+
+
+function App() {
+
+  return (
+    <>
+      <Container>
+        <Row>
+          <span className="text text-primary">test text</span>
+        </Row>
+      </Container>
+    </>
+  )
+}
+
+export default App
